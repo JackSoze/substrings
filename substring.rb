8aa5c4acdf5255ea6e461d8f3558506906146ec9
jacksoze <kamonyojack@gmail.com>
2022-07-27 11:53:13 +0300

    the method work case-insensitively

--- substring.rb
+++ substring.rb
@@ -1,6 +1,8 @@
 def substring(string, dictionary)
+string = string.downcase
 substrings = []
 dictionary.each do |word|
+  word = word.downcase
   if string.include?(word)
     substrings.push(word)
   end
@@ -11,6 +13,6 @@ substrings.reduce(Hash.new(0)) do |result, string|
 end
 end
 
-dictionary = ["below","down","go","going","lo","ing","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","GOING","lo","ing","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 puts substring("Howdy partner, sit down! How's it going?",dictionary)
